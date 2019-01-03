e54a1f980b78494e3d9057e09a039801fd836eb4
Mina Mekhael <mina.mekhael@ymail.com>
2019-01-03 23:23:23 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,19 @@
 #write your code here
 
-def countdown
+def countdown(x)
+  x = 1
+  while x < 60
+  puts "#{x} SECOND(S)!"
+  x += 1
+end 
+"HAPPY NEW YEAR!"
+end
+def countdown_with_sleep(x)
+   x = 1
+  while x < 10
+  sleep (1)
+  puts "#{x} SECOND(S)!"
+  x += 1
+end 
+"HAPPY NEW YEAR!"
 end
